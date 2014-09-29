ceef4557dc68030f0f2373b6d0ddb8f9038d9ec0
Josh Cox <joshcox@indiana.edu>
2014-09-29 02:06:13 -0400

    propagator, update/f, ==, starting unify. TODO: switch to pmatch (all), check name consistency

--- uckanren.rkt
+++ uckanren.rkt
@@ -6,6 +6,9 @@
 
 ;;extend the state with mapping from x -> cell
 (define (ext-s x v s) (cons (cons x `('fresh '() '())) s))
+(define cs-cell caddr)
+(define dom-cell cadr)
+(define val-cell car)
 
 (define (call/fresh f)
   (lambda (s/c)
@@ -36,6 +39,53 @@
 ;;     (let ((s (unify u v (car s/c))))
 ;;       (if s (list (cons s (cdr s/c))) `()))))
 
+(define update/f
+  (lambda (f)
+    (lambda (cell)
+      (lambda (s)
+        (pmatch s
+          (`() `())
+          (`((,aa . ,da) . ,d) `((,aa . ,(f da)) . d))
+          (`(,a . ,d) `(,a . ,(((update/f f) cell) d))))))))
+
+(define (add-constraint c)
+  (update/f (lambda (x) (pmatch x (`(,v ,d ,cs) `(,v ,d ,(cons c cs)))))))
+
+(define (== u v)
+  (lambda (s/c)
+    (let ((c (unify u v)))
+      ((propagate u) (((add-constraint c) v) (((add-constraint c) u) (car s/c)))))))
+
+(define (unify u v)
+  (lambda (s)
+    (lambda (k)
+      'wat)))
+;; u is val, v is val   => same?
+;; [ u is self, v is self => u is v
+;;   u is self, v is val  => u is val
+;;   u is val, v is self  => v is val ]
+;; u is self => u is v
+;; v is self => v is u
+;; u&v are lists => unify car, unify cdr
+
+;; how to make sure that mutiple vars down clash in this unification^^^ (== a b) (== a c) (== a d)
+
+(define p
+  (lambda (cell)
+    (lambda (s)
+      (let ((ls^ (cs-cell (walk cell s))))
+        (letrec ((p (lambda (ls)
+                      (lambda (s)
+                        (lambda (k)
+                          (cond
+                           ((null? ls) (k s))                        
+                           (((car ls) s) k) =>
+                           (lambda (x)
+                             (cond
+                              ((null? x) '())
+                              ((null? (cdr x)) (((p (cdr ls)) s) (car x)))
+                              ((null? (cdr (cdr x))) (((p ls^) (car x)) (car (cdr x))))))))))))
+          (((p ls^) s) (lambda (x) x)))))))
 ;; (define (walk u s)
 ;;   (let ((pr (and (var? u) (assv u s))))
 ;;     (if pr (walk (cdr pr) s) u)))
