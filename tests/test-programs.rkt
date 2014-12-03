87a4935f1dd22d9aec0756620f21d08a1d01905f
Josh Cox <joshcox@indiana.edu>
2014-12-03 15:42:57 -0500

    removed some unnecessary stuff from tests

--- tests/test-programs.rkt
+++ tests/test-programs.rkt
@@ -1,37 +1,8 @@
 #lang racket
-(require "uk.rkt" "mK.rkt" "numbers.rkt")
-(provide (all-defined-out))
+(require "microKanren.rkt")
+(provide (all-defined-out) (all-from-out "microKanren.rkt"))
 
 ;; test programs
-
-;; (define appendo
-;;   (lambda (l s o)
-;;     (conde
-;;      [(== l '()) (== o s)]
-;;      [(fresh (a b res)
-;;              (== l `(,a . ,b))
-;;              (== o `(,a . ,res))
-;;              (appendo b s res))])))
-
-(define listo
-  (lambda (x)
-    (conde
-     [(== x '())]
-     [(fresh (ca cd)
-             (== `(,ca . ,cd) x)
-             (listo cd))])))
-
-(define facto
-  (lambda (n o)
-    (conde
-     [(== n (build-num 1))
-      (== o n)]
-     [(fresh (a b num)
-             (== num (build-num 1))
-             (minuso n num a)
-             (facto a b)
-             (*o b n o))])))
-
 (define reverseo
   (lambda (ls o)
     (conde
@@ -41,33 +12,48 @@
              (reverseo b res)
              (appendo res `(,a) o))])))
 
-(define lengtho
+(define appendo2
+  (lambda (l s o)
+    (disj
+     (conj (== l '()) (== o s))
+     (call/fresh
+      (lambda (a)
+        (call/fresh
+         (lambda (b)
+           (call/fresh
+            (lambda (res)
+              (conj (== l `(,a . ,b))
+                    (conj (== o `(,a . ,res))
+                          (lambda (s/c) (lambda () ((appendo2 b s res) s/c))))))))))))))
+
+(define appendo3
+  (lambda (l s out)
+    (disj
+     (conj (== '() l) (== s out))
+     (call/fresh
+      (lambda (a)
+        (call/fresh
+         (lambda (d)
+           (conj
+            (== `(,a . ,d) l)
+            (call/fresh
+             (lambda (res)
+               (conj
+                (== `(,a . ,res) out)
+                (lambda (s/c)
+                  (lambda ()
+                    ((appendo3 d s res) s/c))))))))))))))
+
+(define reverseo2
   (lambda (ls o)
-    (conde
-     [(== ls '()) (== o (build-num 0))]
-     [(fresh (a d res)
-             (== `(,a . ,d) ls)
-             (lengtho d res)
-             (pluso '(1) res o))])))
-
-(define one-itemo
-  (lambda (x s o)
-    (conde
-     [(== s '()) (== o s)]
-     [(fresh (a b res)
-             (== `(,a . ,b) s)
-             (== o `((,x . ,a) . ,res))
-             (one-itemo x b res))])))
-
-;; (define assqo
-;;   (lambda (x ls o)
-;;     (conde
-;;      [(== ls '()) fail]
-;;      [(fresh (a d aa da)
-;;              (== `(,a . ,d) ls)
-;;              (== `(,aa . ,da) a)
-;;              (== aa x)
-;;              (== o a))]
-;;      [(fresh (a d)
-;;              (== `(,a . ,d) ls)
-;;              (assqo x d o))])))
+    (disj
+     (conj (== ls '()) (== o ls))
+     (call/fresh
+      (lambda (a)
+        (call/fresh
+         (lambda (b)
+           (call/fresh
+            (lambda (res)
+              (conj (== `(,a . ,b) ls)
+                    (conj  (lambda (s/c) (lambda () ((reverseo2 b res) s/c)))
+                           (lambda (s/c) (lambda () ((appendo2 res `(,a) o) s/c))))))))))))))
