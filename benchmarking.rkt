44e82afb5c419bbe5bf46a9f7637b6598e422b95
Josh Cox <joshcox@indiana.edu>
2014-12-09 13:46:21 -0500

    presentation basic and benchmarking stuff

--- benchmarking.rkt
+++ benchmarking.rkt
@@ -36,15 +36,11 @@
 (define ls2 (build-list 200 (lambda (i) (list-ref a (modulo i 26)))))
 (define num1000 (build-num 1000))
 (define num2000 (build-num 2000))
-(define num3000 (build-num 3000))
-(define num4000 (build-num 4000))
+(define num10000 (build-num 10000))
 
 (define microKanren-benchmarks
   (make-benchmark-suite
    "microKanren - nonInterpreter"
-
-   (benchmark "1000^2" (run 1 (q) (fresh (a b) (*o num1000 num1000 a) (exp2 b '() (build-num 50)) (*o a b q))))
-   (benchmark "1000^2" (run 1 (q) (fresh (a b) (pexp2 b '() (build-num 50)) (*o num1000 num1000 a) (*o a b q))))
 
    (benchmark "1000^2" (run 1 (q) (*o num1000 num1000 q)))
    (benchmark "Parellel 1000^2" (run 1 (q) (p*o num1000 num1000 q)))
@@ -52,19 +48,22 @@
    (benchmark "2000^2" (run 1 (q) (*o num2000 num2000 q)))
    (benchmark "Parellel 2000^2" (run 1 (q) (p*o num2000 num2000 q)))
 
-   (benchmark "4000^2" (run 1 (q) (*o num4000 num4000 q)))
-   (benchmark "Parellel 4000^2" (run 1 (q) (p*o num4000 num4000 q)))
+   (benchmark "10000^2" (run 1 (q) (*o num10000 num10000 q)))
+   (benchmark "Parellel 10000^2" (run 1 (q) (p*o num10000 num10000 q)))
    
    (benchmark "two big items"
               (run 2 (q) (disj (reverseo ls50 q) (reverseo ls50 q))))
    (benchmark "two big items"
               (run 2 (q) (pdisj (reverseo ls50 q) (reverseo ls50 q))))
    
+   
+   (benchmark "Reverseo ~~ 25" (run 1 (q) (reverseo ls25 q)))
+   (benchmark "Concurrent Reverseo ~~ 25" (run 1 (q) (preverseo ls25 q)))
+   
+   (benchmark "Reverseo ~~ 50" (run 1 (q) (reverseo ls50 q)))
+   (benchmark "Concurrent Reverseo ~~ 50" (run 1 (q) (preverseo ls50 q)))
+   
    ;(benchmark "Appendo" (run 1 (q) (pappendo2 '(a b) '(c d) q)))
-   ;; (benchmark "Reverseo ~~ 25" (run 1 (q) (reverseo ls25 q)))
-   ;; (benchmark "Concurrent Reverseo ~~ 25" (run 1 (q) (preverseo ls25 q)))
-   ;; (benchmark "Reverseo ~~ 50" (run 1 (q) (reverseo ls50 q)))
-   ;; (benchmark "Concurrent Reverseo ~~ 50" (run 1 (q) (preverseo ls50 q)))
    ;; (benchmark "Reverseo ~~ 100" (run 1 (q) (reverseo ls100 q)))
    ;; (benchmark "Concurrent Reverseo ~~ 100" (run 1 (q) (preverseo ls100 q)))
    ;; (benchmark "Reverseo ~~ 200"(run 1 (q) (reverseo ls2 q)))
