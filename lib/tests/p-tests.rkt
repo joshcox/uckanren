46480d7266fa63723193803cecd533945193d7ac
Josh Cox <joshcox@indiana.edu>
2014-12-12 11:23:19 -0500

    removed benchmarking things into it's own module

--- lib/tests/p-tests.rkt
+++ lib/tests/p-tests.rkt
@@ -1,38 +1,9 @@
 #lang racket
 (require "../miniKanren-base/p-miniKanren-base.rkt"
          "../numbers/p-numbers.rkt"
-         "p-test-programs.rkt")
+         "p-test-programs.rkt"
+         "benchmarking.rkt")
 (provide main)
-
-
-(define-syntax-rule (run/time th) (time-apply th '()))
-(define-syntax-rule (benchmark str bench) (cons str (lambda () bench)))
-(define-syntax-rule (run-benchmark-suite* runner b* ...) 
-  (begin (begin (printf "~a:~n" (car b*)) (run-benchmark-suite b* runner) (newline)) ...))
-(define make-benchmark-suite list)
-
-(define run-benchmark-suite
-  (lambda (benchmarks runner)
-    (let ((suite-name (car benchmarks)))
-      (for ((b (cdr benchmarks)))
-        (begin
-          (let-values (((ans cpu real gc) (run/time (cdr b))))
-              (runner suite-name (car b) (car ans) cpu real gc))
-          (collect-garbage))))))
-
-;; Runners :: (suite-name, bench-name, ans, cpu-time, real-time, gc-time) -> something
-(define-syntax-rule (runner (v ...) exp) (lambda (v ...) exp))
-(define-syntax-rule (runners (v ...) r ...) (lambda (v ...) (begin (r v ...) ...)))
-
-(define std-runner (runner (s n a c r g) (printf "\t~a ~n\t  Cpu: ~a Real: ~a GC: ~a~n" n c r g)))
-(define print-answer (runner (s n a c r g) (printf "\t  ~a~n" a)))
-(define mk-runner (runners (s n a c r g) write-answer std-runner print-answer))
-(define write-answer 
-  (runner (s n a c r g)
-    (let ((f (open-output-file 
-              (build-path (current-directory) "benchmark-results" (string-append n ".txt"))
-              #:exists 'append)))
-      (write r f)(newline f))))
 
 ;; MicroKanren Benchmarking
 (define a '(a b c d e f g h i j k l m n o p q r s t u v w x y z))
--- lib/tests/tests.rkt
+++ lib/tests/tests.rkt
@@ -1,38 +1,9 @@
 #lang racket
 (require "../miniKanren-base/miniKanren-base.rkt"
          "../numbers/numbers.rkt"
-         "test-programs.rkt")
+         "test-programs.rkt"
+         "benchmarking.rkt")
 (provide main)
-
-
-(define-syntax-rule (run/time th) (time-apply th '()))
-(define-syntax-rule (benchmark str bench) (cons str (lambda () bench)))
-(define-syntax-rule (run-benchmark-suite* runner b* ...) 
-  (begin (begin (printf "~a:~n" (car b*)) (run-benchmark-suite b* runner) (newline)) ...))
-(define make-benchmark-suite list)
-
-(define run-benchmark-suite
-  (lambda (benchmarks runner)
-    (let ((suite-name (car benchmarks)))
-      (for ((b (cdr benchmarks)))
-        (begin
-          (let-values (((ans cpu real gc) (run/time (cdr b))))
-              (runner suite-name (car b) (car ans) cpu real gc))
-          (collect-garbage))))))
-
-;; Runners :: (suite-name, bench-name, ans, cpu-time, real-time, gc-time) -> something
-(define-syntax-rule (runner (v ...) exp) (lambda (v ...) exp))
-(define-syntax-rule (runners (v ...) r ...) (lambda (v ...) (begin (r v ...) ...)))
-
-(define std-runner (runner (s n a c r g) (printf "\t~a ~n\t  Cpu: ~a Real: ~a GC: ~a~n" n c r g)))
-(define print-answer (runner (s n a c r g) (printf "\t  ~a~n" a)))
-(define mk-runner (runners (s n a c r g) write-answer std-runner print-answer))
-(define write-answer 
-  (runner (s n a c r g)
-    (let ((f (open-output-file 
-              (build-path (current-directory) "benchmark-results" (string-append n ".txt"))
-              #:exists 'append)))
-      (write r f)(newline f))))
 
 ;; MicroKanren Benchmarking
 (define a '(a b c d e f g h i j k l m n o p q r s t u v w x y z))
@@ -48,11 +19,11 @@
   (make-benchmark-suite
    "microKanren - nonInterpreter"
 
-   (benchmark "Parellel 1000^2" (run 1 (q) (*o num1000 num1000 q)))
+   (benchmark "1000^2" (run 1 (q) (*o num1000 num1000 q)))
 
-   (benchmark "Parellel 2000^2" (run 1 (q) (*o num2000 num2000 q)))
+   (benchmark "2000^2" (run 1 (q) (*o num2000 num2000 q)))
 
-   (benchmark "Parellel 10000^2" (run 1 (q) (*o num10000 num10000 q)))
+   (benchmark "10000^2" (run 1 (q) (*o num10000 num10000 q)))
    
    (benchmark "two big items"
               (run 2 (q) (disj (reverseo ls50 q) (reverseo ls50 q))))
@@ -73,4 +44,3 @@
 
 (define (main)
   (run-benchmark-suite* mk-runner microKanren-benchmarks))
-
