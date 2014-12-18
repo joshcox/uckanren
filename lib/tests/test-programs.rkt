6b5d4e48767c8c572528b791e9bcd45056b86d01
Josh Cox <joshcox@indiana.edu>
2014-12-17 20:42:53 -0500

    moved test-programs to main testing part of lib

--- lib/tests/test-programs.rkt
+++ lib/tests/test-programs.rkt
@@ -68,3 +68,50 @@
               (conj (== `(,a . ,b) ls)
                     (conj  (lambda (s/c) (lambda () ((reverseo2 b res) s/c)))
                            (lambda (s/c) (lambda () ((appendo2 res `(,a) o) s/c))))))))))))))
+
+
+(define listo
+  (lambda (x)
+    (conde
+     [(== x '())]
+     [(fresh (ca cd)
+             (== `(,ca . ,cd) x)
+             (listo cd))])))
+
+(define facto
+  (lambda (n o)
+    (conde
+     [(== n (build-num 1))
+      (== o n)]
+     [(fresh (a b num)
+             (== num (build-num 1))
+             (minuso n num a)
+             (facto a b)
+             (*o b n o))])))
+
+(define reverseo
+  (lambda (ls o)
+    (conde
+     [(== ls '()) (== o ls)]
+     [(fresh (a b res)
+             (== `(,a . ,b) ls)
+             (reverseo b res)
+             (appendo res `(,a) o))])))
+
+(define lengtho
+  (lambda (ls o)
+    (conde
+     [(== ls '()) (== o (build-num 0))]
+     [(fresh (a d res)
+             (== `(,a . ,d) ls)
+             (lengtho d res)
+             (pluso '(1) res o))])))
+
+(define one-itemo
+  (lambda (x s o)
+    (conde
+     [(== s '()) (== o s)]
+     [(fresh (a b res)
+             (== `(,a . ,b) s)
+             (== o `((,x . ,a) . ,res))
+             (one-itemo x b res))])))
